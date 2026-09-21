70e8be53aab08bf7ced8f3022732b00ced0798d3
Gustavo Carneiro <oigustavocarneiro@gmail.com>
2026-09-21 17:49:10 -0300

    Exercicio 2

---
+++ src/Exercicio2.java
@@ -0,0 +1,30 @@
+/*
+2 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar,
+ positivo ou negativo.
+ */
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        if(numero >= 0){
+            System.out.println("O número é positivo");
+        }
+        else{
+            System.out.println("O número é negativo");
+        }
+
+        if(numero % 2 == 0){
+            System.out.print("e par");
+        }
+        else{
+            System.out.print("e impar");
+        }
+
+    }
+}
